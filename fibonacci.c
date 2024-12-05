e47120f590c5a8a20688e85e2f3f7e1ef16104ee
AnnatarM <102091794+AnnatarM@users.noreply.github.com>
2024-12-04 22:08:43 -0600

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n; 
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2); 
+}
+
+int main() {
+    int n;
+    printf("Ingrese el número de términos de la serie Fibonacci: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Ingrese un número mayor a 0.\n");
+        return 1;
+    }
+
+    printf("Serie Fibonacci de los primeros %d términos:\n", n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+    printf("\n");
+    return 0;
+}
